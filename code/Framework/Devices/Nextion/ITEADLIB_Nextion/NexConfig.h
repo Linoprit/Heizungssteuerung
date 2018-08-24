d6fc622440bcbeebb554c0169faeb12e106fa08c
Linoprit <haraldgraef@web.de>
2018-08-24 16:11:42 +0200

    repair ITEADLIB

---
+++ code/Framework/Devices/Nextion/ITEADLIB_Nextion/NexConfig.h
@@ -0,0 +1,64 @@
+/**
+ * @file NexConfig.h
+ *
+ * Options for user can be found here. 
+ *
+ * @author  Wu Pengfei (email:<pengfei.wu@itead.cc>)
+ * @date    2015/8/13
+ * @copyright 
+ * Copyright (C) 2014-2015 ITEAD Intelligent Systems Co., Ltd. \n
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+#ifndef __NEXCONFIG_H__
+#define __NEXCONFIG_H__
+
+#include <System/Error_messaging.h>
+#include "string.h"
+#include "strings.h"
+
+
+/**
+ * @addtogroup Configuration 
+ * @{ 
+ */
+
+/** 
+ * Define DEBUG_SERIAL_ENABLE to enable debug serial. 
+ * Comment it to disable debug serial. 
+ */
+//#define DEBUG_SERIAL_ENABLE
+
+/**
+ * Define dbSerial for the output of debug messages. 
+ */
+#define dbSerial Serial
+
+/**
+ * Define nexSerial for communicate with Nextion touch panel. 
+ */
+#define nexSerial Common::nex_disp
+#define millis	  Common::get_tick
+
+
+#ifdef DEBUG_SERIAL_ENABLE
+/*#define dbSerialPrint(a)    dbSerial.print(a)
+#define dbSerialPrintln(a)  dbSerial.println(a)
+#define dbSerialBegin(a)    dbSerial.begin(a)*/
+
+#define dbSerialPrint(a)    WRITE_TRACE(a)
+#define dbSerialPrintln(a)  WRITE_TRACE(a)
+#define dbSerialBegin(a)
+#else
+#define dbSerialPrint(a)    do{}while(0)
+#define dbSerialPrintln(a)  do{}while(0)
+#define dbSerialBegin(a)    do{}while(0)
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* #ifndef __NEXCONFIG_H__ */
